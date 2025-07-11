9fea531fb927b8cfb849ca230394175dbb59cd46
Dharmendra Pathak <161156026+dharmendrapathak008@users.noreply.github.com>
2025-07-11 17:37:02 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,3 +13,7 @@ self.addEventListener('notificationclick', function (event) {
   event.notification.close();
   event.waitUntil(clients.openWindow(event.notification.data.url));
 });
+
+self.addEventListener('fetch', function(event) {
+  // Required to enable PWA installability
+});
